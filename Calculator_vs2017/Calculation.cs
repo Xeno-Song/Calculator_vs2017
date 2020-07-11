52f4a9e12df49b386772ecc2c10f0388db44d7fe
Xeno <mikybear@naver.com>
2020-07-11 23:44:16 +0900

    Function implementation

---
+++ Calculator_vs2017/Calculation.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_vs2017
+{
+	public enum Operator
+	{
+		Plus,
+		Minus,
+		Multiply,
+		Devide
+	}
+
+	class Calculation
+	{
+		public double num1 { get; protected set; }
+		public double num2 { get; protected set; }
+		public Operator calc_operator { get; protected set; }
+
+		public Calculation()
+		{
+			num1 = 0;
+			num2 = 0;
+		}
+
+		public void SetFirstNum(double _num1)
+		{
+			num1 = _num1;
+		}
+
+		public void SetFirstNum(int _num1)
+		{
+			num1 = (double)_num1;
+		}
+
+		public void SetSecondNum(double _num2)
+		{
+			num2 = _num2;
+		}
+
+		public void SetSecondNum(int _num2)
+		{
+			num2 = (double)_num2;
+		}
+
+		public void SetOperator(Operator _calc_operator)
+		{
+			calc_operator = _calc_operator;
+		}
+
+		public double GetCalculationResult()
+		{
+			double result = 0.0;
+
+			switch(calc_operator)
+			{
+				case Operator.Plus:
+				{
+					result = num1 + num2;
+					break;
+				}
+
+				case Operator.Minus:
+				{
+					result = num1 - num2;
+					break;
+				}
+
+				case Operator.Multiply:
+				{
+					result = num1 * num2;
+					break;
+				}
+
+				case Operator.Devide:
+				{
+					if(Math.Abs(num2) < 0.000000001)
+					{
+						MessageBox.Show("Cannot device to 0");
+						break;
+					}
+
+					result = num1 / num2;
+					break;
+				}
+			}
+
+			return result;
+		}
+	}
+}
--- Calculator_vs2017/Form1.cs
+++ Calculator_vs2017/Form1.cs
@@ -12,9 +12,137 @@ namespace Calculator_vs2017
 {
 	public partial class Form1 : Form
 	{
+		public bool is_buffered;
+		public int num;
+		public int num_buffer;
+
+		Calculation calc;
+
 		public Form1()
 		{
 			InitializeComponent();
+
+			num = 0;
+			is_buffered = false;
+			num_buffer = 0;
+
+			SetTextOutput(0);
+			calc = new Calculation();
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+			AddNumBelow(3, ref num);
+		}
+
+		private void button11_Click(object sender, EventArgs e)
+		{
+			AddNumBelow(0, ref num);
+		}
+
+		private void button10_Click(object sender, EventArgs e)
+		{
+			AddNumBelow(1, ref num);
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+			AddNumBelow(2, ref num);
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			AddNumBelow(4, ref num);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			AddNumBelow(5, ref num);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			AddNumBelow(6, ref num);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			AddNumBelow(7, ref num);
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			AddNumBelow(8, ref num);
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			AddNumBelow(9, ref num);
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+			SetNumToBuffer();
+			calc.SetOperator(Operator.Plus);
+		}
+
+		private void button12_Click(object sender, EventArgs e)
+		{
+			SetNumToBuffer();
+			calc.SetOperator(Operator.Minus);
+		}
+
+		private void button13_Click(object sender, EventArgs e)
+		{
+			SetNumToBuffer();
+			calc.SetOperator(Operator.Multiply);
+		}
+
+		private void button14_Click(object sender, EventArgs e)
+		{
+			SetNumToBuffer();
+			calc.SetOperator(Operator.Devide);
+		}
+
+		private void button15_Click(object sender, EventArgs e)
+		{
+			double result = 0.0;
+			SetNumToBuffer();
+			result = calc.GetCalculationResult();
+
+			SetTextOutput(result);
+		}
+
+		private int AddNumBelow(int _num, ref int result)
+		{
+			result = result * 10 + _num;
+			SetTextOutput(result);
+
+			return result;
+		}
+
+		private void SetNumToBuffer()
+		{
+			if (is_buffered)
+				calc.SetSecondNum(num);
+			else
+				calc.SetFirstNum(num);
+
+			is_buffered = true;
+			SetTextOutput(0);
+
+			num_buffer = num;
+			num = 0;
+		}
+
+		private void SetTextOutput(int _num)
+		{
+			ResultText.Text = _num.ToString();
+		}
+
+		private void SetTextOutput(double _num)
+		{
+			ResultText.Text = _num.ToString();
 		}
 	}
 }
